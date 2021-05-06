48879ab998e054998b3fca22e5766f25f3d9c1d1
Carlos Vinicius <69722024+CarlossViniciuss@users.noreply.github.com>
2021-05-06 00:03:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ app.use(require('./routes/'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 //servidor
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server on port 3000');
 });
